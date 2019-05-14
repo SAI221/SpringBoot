83a4784cc8bedb3587ab4992c6737912c01c0d63
niksm96 <mundargi95@gmail.com>
2019-05-14 10:15:19 +0530

    14-05

--- SpringBootLoginRegistration/src/main/java/com/bridgelabz/springbootform/controller/MailController.java
+++ SpringBootLoginRegistration/src/main/java/com/bridgelabz/springbootform/controller/MailController.java
@@ -20,7 +20,7 @@ import com.bridgelabz.springbootform.model.UserDetails;
 import com.bridgelabz.springbootform.service.UserService;
 
 @RestController
-public class ForgotPasswordController {
+public class MailController {
 
 	@Autowired
 	private JavaMailSender sender;
@@ -46,37 +46,23 @@ public class ForgotPasswordController {
 	}
 
 	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
-	public String forgotPassword(@RequestBody UserDetails user, HttpServletRequest request,HttpServletResponse response) {
+	public String forgotPassword(@RequestBody UserDetails user, HttpServletRequest request,
+			HttpServletResponse response) {
 		List<UserDetails> list = userService.findByEmailId(user.getEmail());
 		if (list.size() == 0) {
 			return "We didn't find an account for that e-mail address.";
 		} else {
 			UserDetails userdetails = list.get(0);
-			String token=userService.jwtToken(userdetails.getUserId());
-			response.setHeader("token",token );
+			String token = userService.jwtToken(userdetails.getUserId());
+			response.setHeader("token", token);
 
-			/*
-			 * String appUrl = request.getScheme() + "://" + request.getServerName();
-			 * 
-			 * // Email message SimpleMailMessage passwordResetEmail = new
-			 * SimpleMailMessage();
-			 * passwordResetEmail.setFrom("saipravallika2097@gmail.com");
-			 * passwordResetEmail.setTo(userdetails.getEmail());
-			 * passwordResetEmail.setSubject("Password Reset Request");
-			 * passwordResetEmail.setText("To reset your password, click the link below:\n"
-			 * + appUrl + "/reset?token=");
-			 * 
-			 * sender.send(passwordResetEmail); return
-			 * "A password reset link has been sent to " + user.getEmail();
-			 */
 			MimeMessage message = sender.createMimeMessage();
 			MimeMessageHelper helper = new MimeMessageHelper(message);
 			String appUrl = request.getScheme() + "://" + request.getServerName();
 			try {
 
 				helper.setTo(userdetails.getEmail());
-				helper.setText("To reset your password, click the link below:\n"
-						 + appUrl + "/reset?token="+token);
+				helper.setText("To reset your password, click the link below:\n" + appUrl + "/reset?token=" + token);
 				helper.setSubject("Password Reset Request");
 			} catch (MessagingException e) {
 				e.printStackTrace();
@@ -90,19 +76,61 @@ public class ForgotPasswordController {
 	@RequestMapping(value = "/reset", method = RequestMethod.PUT)
 	public String changePassword(HttpServletRequest request, @RequestBody String password) {
 		String token = request.getHeader("token");
-		
-			int id = userService.parseJWT(token);
-			if (id >=0) {
+
+		int id = userService.parseJWT(token);
+		if (id >= 0) {
 			Optional<UserDetails> userList = userService.findById(id);
 			userList.get().setPassword(password);
 			userService.UserRegistration(userList.get());
 			return "Changed";
-		}
-		else
+		} else
 			return "Not changed";
-		
 
 	}
-	 
+	
+	@RequestMapping(value="/mail", method=RequestMethod.POST)
+	public String mailForActivation(@RequestBody UserDetails user,HttpServletRequest request,HttpServletResponse response)
+	{
+		List<UserDetails> list = userService.findByEmailId(user.getEmail());
+		if (list.size() == 0) {
+			return "We didn't find an account for that e-mail address.";
+		} else {
+			UserDetails userdetails = list.get(0);
+			String token = userService.jwtToken(userdetails.getUserId());
+			response.setHeader("token", token);
+
+			MimeMessage message = sender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message);
+			String appUrl = "http://localhost:8080";
+			try {
+
+				helper.setTo(userdetails.getEmail());
+				helper.setText("To reset your password, click the link below:\n" + appUrl + "/active/token=" + token);
+				helper.setSubject("Password Reset Request");
+			} catch (MessagingException e) {
+				e.printStackTrace();
+				return "Error while sending mail ..";
+			}
+			sender.send(message);
+			return "Mail Sent Success!";
+		}
+		
+		
+	}
+	
+	@RequestMapping(value="/active", method=RequestMethod.PUT)
+	public String activeStatus(HttpServletRequest request)
+	{
+		String token = request.getHeader("token");
+
+		int id = userService.parseJWT(token);
+		if (id >= 0) {
+			Optional<UserDetails> userList = userService.findById(id);
+			userList.get().setActiveStatus(1);
+			userService.UserRegistration(userList.get());
+			return "Changed";
+		} else
+			return "Not changed";
+	}
 
 }
